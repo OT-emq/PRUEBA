c6a3c5648d168482a20c4354f409d4d89b1758b8
Stiven <almeidaibarra.stiven@gmail.com>
2026-06-23 22:37:33 -0500

    header base terminado

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,13 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/app.css')
 </head>
 
 <body>
-    <div class="">
-        <img class="h-20" src="{{ asset('images/logo.png') }}" alt="Logo">
-        
+    <div class="bg-[#8DC2FF] flex p-1">
+        <img class="h-20 self-center" src="{{ asset('images/logo.png') }}" alt="Logo">
+        <h1 class="font-bold text-white text-[20px] mx-auto self-center">GENERACION PARA ORDEN DE TRABAJO (OT)</h3>
+    </div>
+ 
+    <div class="flex mx-auto max-w-200">
+       
+        <button class="flex-1 m-3 text-white border border-black bg-[#6ADA6E] rounded-[7px] py-1 px-3 font-bold">Inicio</button>
+        <button class="flex-1 m-3 text-white border border-black bg-[#6ADA6E] rounded-[7px] py-1 px-3 font-bold">Trabajadores</button>
+        <button class="flex-1 m-3 text-white border border-black bg-[#6ADA6E] rounded-[7px] py-1 px-3 font-bold">Plan diario</button>
+        <button class="flex-1 m-3 text-white border border-black bg-[#6ADA6E] rounded-[7px] py-1 px-3 font-bold">Actividades</button>
+       
     </div>
 
 </body>
